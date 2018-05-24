939ee78d5749cd6f082d37fe9b8031c5ba271e56
NexturnZ <ZhouLKmail@gmail.com>
2018-05-24 13:00:06 +0800

    version2.1: 1.more screenshots are added to test; 2.successfully extract edge in hue image;
    Next step:
    find a better way to locate center of next stage.

--- RGBtoHSI.m
+++ RGBtoHSI.m
@@ -1,16 +1,20 @@
-function [H,S,I] = RGBtoHSI(pixel)
-R = pixel(1);
-G = pixel(2);
-B = pixel(3);
+function I_hsi = RGBtoHSI(Image)
+s = size(Image);
+R = Image(:,:,1);
+G = Image(:,:,2);
+B = Image(:,:,3);
 % Hue calculation
-theta = acos(0.5*(R-G+R-B)/sqrt((R-G)^2+(R-B)*(G-B)));
-if B<=G
-    H = theta/pi*180;
-else
-    H = 360-theta/pi*180;
-end
+H = zeros(s(1),s(2));
+theta = acos(0.5*(R-G+R-B)./sqrt(0.1+(R-G).^2+(R-B).*(G-B)));
+H(B<=G) = theta(B<=G)/pi*180;
+H(B>G)  = 360-theta(B>G)/pi*180;
 % Saturate calculation
-S = 1-3*min(pixel)/sum(pixel);
+S = 1-3*min(Image,[],3)./sum(Image,3);
 % intensity calculation
-I = mean(pixel);
+I = mean(Image,3);
+
+I_hsi = zeros(s);
+I_hsi(:,:,1) = H;
+I_hsi(:,:,2) = S;
+I_hsi(:,:,3) = I;
 end
--- Test1.m
+++ Test1.m
@@ -1,17 +1,17 @@
 clear all; close all;
 dbstop if error;
-I = double(imread('Screenshot1.png'));
-
+I = double(imread('Screenshot10.png'));
+draw(I);title('origin');
 s = size(I);
 %% transfer RGB to HSI
 
-I_hsi = zeros(s(1),s(2),3);
-for i1 = 1:s(1)
-   for i2 = 1:s(2)
-      [I_hsi(i1,i2,1),I_hsi(i1,i2,2),I_hsi(i1,i2,3)] = RGBtoHSI(I(i1,i2,:)); 
-   end
-end
-draw(I);title('origin');
+% I_hsi = zeros(s(1),s(2),3);
+% for i1 = 1:s(1)
+%    for i2 = 1:s(2)
+%       [I_hsi(i1,i2,1),I_hsi(i1,i2,2),I_hsi(i1,i2,3)] = RGBtoHSI(I(i1,i2,:)); 
+%    end
+% end
+I_hsi = RGBtoHSI(I);
 draw(I_hsi(:,:,1));title('Hue');
 % draw(I_hsi(:,:,2));title('Saturate');
 % draw(I_hsi(:,:,3));title('Intensity');
@@ -19,10 +19,12 @@ draw(I_hsi(:,:,1));title('Hue');
 G = [-1,-1,-1;...
      -1, 8,-1;...
      -1,-1,-1];
-O = mask_conv(I_hsi(:,:,1),G);
-% O(O>=10)=255;
-% O(O<10) = 0;
-draw(O(2:end-1,2:end-1));
+O = mask_conv(I_hsi(:,:,3),G);
+Iplot = abs(O);
+Iplot = Iplot/max(Iplot(:))*255;
+Iplot(O>=20)=255;
+Iplot(O<20) = 0;
+draw(Iplot(2:end-1,2:end-1));
 
 %% circle finding 
 r = 30;
